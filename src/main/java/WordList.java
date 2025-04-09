34f4b06c48c1f1fed933abaeb5a38726d7ca508a
manny1040 <mabercian13@gmail.com>
2025-04-09 10:07:27 -0700

    Update WordList.java

--- src/main/java/WordList.java
+++ src/main/java/WordList.java
@@ -21,11 +21,14 @@ public class WordList
   public void removeWordsOfLength(int len)
   {
     /* to be implemented in part (b) */
-    for(int i = 0; i < myList.size(); i++){
-      if(myList.get(i).length() == len){
+    for(int i = 0; i < myList.size(); i++)
+      {
+      if(myList.get(i).length() == len)
+        {
         myList.remove(i);
         i--;
+          
       }
-    } 
+    }
   }
 }
